71bf3782c9a9c8810220490a0501d4e0c0073017
Tamara Piksa <tamara.piksa@gmail.com>
2016-08-15 11:17:06 -0700

    Vampire numbers

--- kata.js
+++ kata.js
@@ -174,3 +174,34 @@ exports.maxProduct = function maxProduct(numbers) {
   }
   return currentMax * lastMax
 }
+
+
+
+/*Vampire Numbers
+
+Our loose definition of Vampire Numbers can be described as follows:
+
+6 * 21 = 126
+# 6 and 21 would be valid 'fangs' for a vampire number as the 
+# digits 6, 1, and 2 are present in both the product and multiplicands
+
+10 * 11 = 110
+# 110 is not a vampire number since there are three 1's in the
+# multiplicands, but only two 1's in the product
+Create a function that can receive two 'fangs' and determine if the product of 
+the two is a valid vampire number.
+
+http://www.codewars.com/kata/54d418bd099d650fa000032d/train/javascript
+*/
+
+var vampire_test = function(a, b){
+  let fangs = [a, b];
+  let product = a * b;
+  if ( fangs.each === product.each ) {
+    return true;
+  } else {
+    return false;
+  }
+}
+
+
